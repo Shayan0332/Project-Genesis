995ee051cb5e1427ced28f98b82331493d192a11
Shayan Riaz <shayanmlk123@gmail.com>
2026-02-14 21:33:11 +0500

    Day 1: Input/Output practice file created

---
+++ day1_io.py
@@ -0,0 +1,9 @@
+print("Project Genesis - Day 1: Input & Output Practice")
+
+age = input("How old are you? ")
+age = int(age)
+print("Next year, you will be", age + 1)
+
+height = input("What is your height in cm? ")
+height = float(height)
+print("Wow,", height, "cm tall!")
